e924dbc40fae7322277a9066a0bd00914ff20dfe
Eve <yihu@andrew.cmu.edu>
2019-11-04 13:42:35 -0800

    added arg parser to the training program

--- Piwho/trainingourselves.py
+++ Piwho/trainingourselves.py
@@ -1,15 +1,20 @@
 from piwho import recognition
+import argparse
 
-recog = recognition.SpeakerRecognizer("/home/pi/Projects/voiceProject/models/eve")
-recog.speaker_name ='Eve'
-recog.train_new_data()
 
-recog = recognition.SpeakerRecognizer('/home/pi/Projects/voiceProject/models/akansha')
-recog.speaker_name ='Akansha'
-recog.train_new_data()
+parser = argparse.ArgumentParser()
+parser.add_argument("name", help="the name for the training model")
+parser.add_argument("-f", help="(optional) specific file for training")
+args = parser.parse_args()
 
-recog = recognition.SpeakerRecognizer('/home/pi/Projects/voiceProject/models/JAWAHAR')
-recog.speaker_name ='Jawahar'
-recog.train_new_data()
+model_path_dir = '/home/pi/Projects/voiceProject/models/'
+
+def train_speaker(name, *args):
+    recog = recognition.SpeakerRecognizer('{0}{1}'.format(model_path_dir,name))
+    recog.speaker_name = name
+    if(len(args)==1):
+        recog.train_new_data(args[0])
+
+train_speaker(args.name)
 
 print recog.get_speakers()
